182ba2dd451d049aea02f9ecb3b005f285023e81
Johnnyli-cloud <m13852196272@163.com>
2024-11-19 15:33:28 +0800

    solidity 变量

---
+++ contracts/SolidityTest.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract SolidityTest {
+    uint256 public storedData; // 状态变量
+
+    function getResult() public pure returns (uint256) {
+        uint256 a = 1; // 局部变量
+        uint256 b = 2;
+        uint256 result = a + b;
+        return result; // 访问局部变量
+    }
+}
