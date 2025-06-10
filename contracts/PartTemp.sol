d81bf85d01fae6bda44152a6f545179fa016da05
Johnnyli-cloud <m13852196272@163.com>
2025-06-10 20:41:15 +0800

    局部变量

---
+++ contracts/PartTemp.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+// 局部变量
+contract PartTemp {
+    uint256 storedData; // 状态变量
+
+    constructor() public {
+        storedData = 10;
+    }
+
+    function getResult() public view returns (uint256) {
+        uint256 a = 1; // 局部变量
+        uint256 b = 2;
+        uint256 result = a + b;
+        return result; // 访问局部变量
+    }
+}
